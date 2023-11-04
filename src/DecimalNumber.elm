28d15f52632815d4c6fe212cfb56ca4f42fe451a
Philipp Middendorf <philipp.middendorf@desy.de>
2023-11-04 14:24:56 +0100

    Last version with numeric-decimal

--- src/DecimalNumber.elm
+++ src/DecimalNumber.elm
@@ -1,8 +1,9 @@
-module DecimalNumber exposing (DecimalNumber)
+module DecimalNumber exposing (DecimalNumber, fromInt, fromIntegralAndDecimals, integralPart, toString)
 
+import List exposing (foldl)
 import Numeric.Decimal as NumericDecimal exposing (Decimal)
 import Numeric.Decimal.Rounding exposing (RoundingAlgorythm(..))
-import Numeric.Nat exposing (nat0, toInt)
+import Numeric.Nat exposing (fromIntAbs, toInt)
 import Numeric.Rational as Rational
 
 
@@ -12,3 +13,69 @@ type MyDecimals
 
 type alias DecimalNumber =
     Decimal MyDecimals Int
+
+
+type alias Precision =
+    Int
+
+
+integralPart : DecimalNumber -> Int
+integralPart =
+    Tuple.first << NumericDecimal.splitDecimal
+
+
+decimalPart : DecimalNumber -> Int
+decimalPart =
+    Tuple.second << NumericDecimal.splitDecimal
+
+
+getPrecision : DecimalNumber -> Precision
+getPrecision =
+    toInt << NumericDecimal.getPrecision
+
+
+toString : DecimalNumber -> String
+toString =
+    NumericDecimal.toString
+
+
+lt : DecimalNumber -> DecimalNumber -> Bool
+lt x y =
+    Rational.lessThan (NumericDecimal.toRational x) (NumericDecimal.toRational y)
+
+
+gt : DecimalNumber -> DecimalNumber -> Bool
+gt x y =
+    Rational.greaterThan (NumericDecimal.toRational x) (NumericDecimal.toRational y)
+
+
+fromInt : Precision -> Int -> DecimalNumber
+fromInt precision number =
+    NumericDecimal.fromInt RoundDown (fromIntAbs precision) number
+
+
+fromIntegralAndDecimals : Precision -> Int -> List Int -> DecimalNumber
+fromIntegralAndDecimals precision integral decimals =
+    NumericDecimal.succeed
+        RoundDown
+        (fromIntAbs precision)
+        (integral * 10 ^ precision + foldl (\new old -> old * 10 + new) 0 decimals)
+
+
+tenToThePowerMinus : Precision -> Int -> DecimalNumber
+tenToThePowerMinus precision whichDigit =
+    NumericDecimal.succeed
+        RoundDown
+        (fromIntAbs precision)
+        (10 ^ (precision - 1 - whichDigit))
+
+
+flipSign : DecimalNumber -> DecimalNumber
+flipSign value =
+    let
+        minusOne =
+            NumericDecimal.fromInt RoundDown (NumericDecimal.getPrecision value) -1
+    in
+    NumericDecimal.multiply
+        minusOne
+        value
--- tests/Example.elm
+++ tests/Example.elm
@@ -1,12 +1,51 @@
 module Example exposing (..)
 
-import DigitalNumber as DN
+import DecimalNumber as Decimal
 import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
-import Test exposing (Test, describe)
+import Fuzz exposing (int, intRange, list, listOfLengthBetween)
+import List
+import Test exposing (Test, describe, fuzz, test)
 
 
 suite : Test
 suite =
-    describe "digital numbers"
-        [ test "output without sign and decimals" (\_ -> DN.make minValue maxValue currentValue) ]
+    describe "decimal numbers"
+        [ fuzz int
+            "from int to string (fuzzed)"
+            (\x ->
+                Expect.equal
+                    (String.fromInt x ++ ".00")
+                    (Decimal.toString (Decimal.fromInt 2 x))
+            )
+        , test "from integral and decimals to string" (\_ -> Expect.equal "123.45" (Decimal.toString (Decimal.fromIntegralAndDecimals 2 123 [ 4, 5 ])))
+        , test "from integral and decimals to string with -1.1" (\_ -> Expect.equal "-1.1" (Decimal.toString (Decimal.fromIntegralAndDecimals 1 -1 [ 1 ])))
+        , fuzz (listOfLengthBetween 0 13 (intRange 0 9))
+            "from integral and decimals to string (fuzzed)"
+            (\xs ->
+                let
+                    value =
+                        Decimal.toString (Decimal.fromIntegralAndDecimals (List.length xs) 123 xs)
+                in
+                if List.isEmpty xs then
+                    Expect.equal "123" value
+
+                else
+                    Expect.equal ("123." ++ String.join "" (List.map String.fromInt xs)) value
+            )
+        , fuzz
+            (Fuzz.map2
+                (\decimalDigits integralPart -> ( decimalDigits, integralPart ))
+                (listOfLengthBetween 0 13 (intRange 0 9))
+                int
+            )
+            "get decimal integral part"
+            (\( decimalDigits, integralPart ) ->
+                Expect.equal integralPart
+                    (Decimal.integralPart <|
+                        Decimal.fromIntegralAndDecimals
+                            (List.length decimalDigits)
+                            integralPart
+                            decimalDigits
+                    )
+            )
+        ]
